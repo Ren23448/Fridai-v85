76fdea489bb56ffe069ecb66f2220f8a0f5fcd9e
Ren23448 <yayurir@gmail.com>
2024-11-23 22:31:29 -0800

    Update app-auther.js

--- helpers/constants/app-auther.js
+++ helpers/constants/app-auther.js
@@ -1,15 +1,13 @@
 export const app_author = `data = {
-  name: "Omar",
-  fullName: "Omar Abdulaziz",
-  age: "10/12/1999",
-  location: "Saudi Arabia",
-  siteUrl: "https://omar11.sa/",
-  github: "https://github.com/omarx11",
-  twitter: "https://twitter.com/@dis_x0",
-  email: "mail@omar11.sa",
-  discordId: "582305812903493663",
-  omar_description:
-    "A self-taught web developer and a creator of Rawan, loves developing with javascript and making websites, A student from TVTC College education.",
+  name: "Ren",
+  fullName: "Ren yayuri",
+  age: "09/12/2007",
+  location: "US Washington state",
+  siteUrl: "https://ghost.com/",
+  github: "https://github.com/Ren23448/Fridai-v85",
+  email: "yayir@gmail.com",
+  Ren_description:
+    "A self-taught web developer and a creator of Ren, loves developing with html,css,js,py and making websites.",
   rawan_description:
-    "Rawan is an AI girl who is developed by Omar using the technology on the web. Why?, Well, it's just for fun, or maybe to improve his programming skills.",
+    "Fridai is an AI girl who is developed by Rej using the technology on the web. Why?, Well, it's just for fun, or maybe to improve his programming skills.",
 }`;
